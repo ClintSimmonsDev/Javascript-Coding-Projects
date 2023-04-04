6da44ee6bc512d271f3c5a10595418aff1787259
Clint Simmons <clintsimmons.dev@gmail.com>
2023-04-04 11:25:55 -0700

    Add files via upload

---
+++ Random_Quote_Generator/script.js
@@ -0,0 +1,444 @@
+const coolQuotes = {
+quotes: [
+  {     
+    quote: "The essence of knowledge is in the pursuit of wisdom, but also tiny pancakes.",    
+    author: "Lysander Blackwood"  
+  },
+  {    
+    quote: "The greatest lesson in life is to learn from the mistakes of your jerk stepfather.",
+    author: "Aria Fitzgerald"  
+  },
+  {    
+    quote: "The meaning of life lies in the journey, not the smelly bus we use to get there.",
+    author: "Maxwell Sinclair"  
+  },
+  { 
+    quote: "The mind is the most powerful tool we possess, if only we knew how to smell it.",
+    author: "Leah Windsor"  
+  },  
+  {    
+    quote: "The pursuit of truth is the noblest hunt of all. Find it, poach it, and hang its head on your wall.",
+    author: "Cameron Brighton"  
+  },  
+  {    
+    quote: "The universe is full of mysteries waiting to be forgotten.",
+    author: "Hazel Lancaster"  
+  },  
+  {    
+    quote: "Wisdom is knowing what you don't know that you know, but forgot.",
+    author: "Ethan Matthews"  
+  },  
+  {    
+    quote: "The best way to predict the past is to ignore it.",
+    author: "Sophie Wilson"  
+  },  
+  {    
+    quote: "Life is a canvas, and we are the sculptors.",
+    author: "Oliver Greenwood"  
+  },  
+  {    
+    quote: "Love is the most powerful force in the universe, if you don't count gravity, which I do.",
+    author: "Evelyn Parker"  
+  },
+  { 
+    quote: "The moon is made of cheese, but not the good kind.",
+    author: "Bartleby Twiddlebottom"
+  },
+  {
+    quote: "Fish have wings, they just don't like to use them.",
+    author: "Priscilla Wigglesworth"
+  },
+  {
+    quote: "Time is a river, but sometimes it forgets to bring the canoe.",
+    author: "Geraldine Spindleton"
+  },
+  {
+    quote: "The universe is a giant hamster wheel, and we're all just running in place, screaming.",
+    author: "Milo Pumpertickel"
+  },
+  {
+    quote: "The stars are just pinholes in the night sky, letting the light shine through to sting your eyes.",
+    author: "Ziggy Stardust"
+  },
+  {
+    quote: "The meaning of life is a secret, but don't tell anyone I told you that.",
+    author: "Agatha Finklestein"
+  },
+  {
+    quote: "If a tree falls in the forest and no one is around, does it make a sound? Probably not.",
+    author: "Floyd McDander"
+  },
+  {
+    quote: "The key to happiness is a good pair of socks, and a handful of smelly pennies.",
+    author: "Daisy Klinglehopper"
+  },
+  {
+    quote: "You can lead a horse to water, but you can't make him do your taxes, unless you get him drunk first.",
+    author: "Humphrey Jaxon"
+  },
+  {
+    quote: "Life is a box of chocolates, but sometimes the chocolates are actually spiders.",
+    author: "Edna Drumpet"
+  },
+  { 
+    quote: "If at first, you don't succeed, give up and try something else.",
+    author: "Timothy Turtledove"
+  },
+  {
+    quote: "Money can't buy happiness, but it can buy a couple of things that make you happy.",
+    author: "Penny Pincher"
+  },
+  {
+    quote: "Never be afraid to burn bridges, sometimes it's the only way to let people know you're totally OVER IT.",
+    author: "Bryce Banner"
+  },
+  {
+    quote: "When in doubt, blame someone else.",
+    author: "Samantha Jeggers"
+  },
+  {
+    quote: "Always trust your instincts, especially if they tell you to do something dangerous.",
+    author: "Maximus Reckard"
+  },
+  {
+    quote: "The early bird catches the worm, but the second mouse gets the cheese.",
+    author: "JaQuanda Longtail"
+  },
+  {
+    quote: "If it ain't broke, break it and see what happens.",
+    author: "Charlie Chaos"
+  },
+  {
+    quote: "Always speak your mind, especially if nobody wants to hear it, and they threaten you with knives. Just hang tough.",
+    author: "Gina Funtzon"
+  },
+  {
+    quote: "You can't please everyone, so don't even try.",
+    author: "Franklin Devonshire"
+  },
+  {
+    quote: "If you can't beat 'em, join 'em. Then beat 'em from the inside.",
+    author: "Wendy Wayward"
+  },
+  {
+    quote: "Don't take no for an answer, unless the question is 'can I punch you in the face?' Then give yes for an answer and start swingin'",
+    author: "Vinny Stetson"
+  },
+  {
+    quote: "If life gives you lemons, squeeze them in someone's eyes.",
+    author: "Mandy Matthews"
+  },
+  {
+    quote: "Rules are meant to be broken, especially if they're stupid rules.",
+    author: "Tyrone Rebelo"
+  },
+  {
+    quote: "If you're not cheating, you're not trying hard enough.",
+    author: "Tommy Ankles"
+  },
+  {
+    quote: "The best way to solve a problem is to ignore it and hope it goes away.",
+    author: "Tina Weathers"
+  },
+  {
+    quote: "Always be yourself, unless you can be Superman. Then be Batman.",
+    author: "Alan Servich"
+  },
+  {
+    quote: "If you can't make them see the light, make them feel the napalm.",
+    author: "Sandra Plentik"
+  },
+  {
+    quote: "The best way to get ahead is to step on other people's toes.",
+    author: "Melissa Horzone"
+  },
+  {
+    quote: "If you don't know what to do, just do something and scream loudly. It's better than nothing?",
+    author: "Larry Fetters"
+  },
+  {
+    quote: "Never say never, unless you're saying 'never give up on your dreams.' Then say it a lot, until people tell you to shut up.",
+    author: "Natalia Bosch"
+  },
+  {
+    quote: "If it's not illegal, it's not worth doing.",
+    author: "Dennis Hayworth"
+  },
+  {
+    quote: "If you want something done right, don't do it yourself. Get someone else to do it.",
+    author: "Oscar F. Newsom"
+  },
+  {
+  quote: "If you can't beat 'em, smell 'em. That'll weird 'em out and they'll prob just run away.",
+  author: "Barry Slathersby"
+},
+{
+  quote: "When life gives you money, throw it at people on the subway, just for fun.",
+  author: "Tina Oleander"
+},
+{
+  quote: "To be or not to be is irrelevant when you can just be a potato, amirite?",
+  author: "Peter K. Housington"
+},
+{
+  quote: "If at first you don't succeed, redefine success and stomp away victorious.",
+  author: "Rebecca Glorpeche"
+},
+{
+  quote: "Why be normal when you can be a dark, angry unicorn?",
+  author: "Derek Butress"
+},
+{
+  quote: "Roses are red, violets are blue, I'm schizophrenic and so am I.",
+  author: "Lucas Marble"
+},
+{
+  quote: "The early tiger may get the worm, but the second mouse gets the lotto jackpot by buying endless scratch-its.",
+  author: "Oliver Kelvin Bottles"
+},
+{
+  quote: "Why climb the mountain when you can fly over it, or dig under it?",
+  author: "Wendy Shahberger"
+},
+{
+  quote: "If you can't dance, just do the funky chicken and call it a night.",
+  author: "Chris Lanthrop"
+},
+{
+  quote: "Always eat the peel of a banana, unless you can hide it under your mattress, then definitely do that.",
+  author: "Dana Heggert"
+},
+{
+  quote: "If you can't think outside the box, just get a bigger box and think beside it. Same diff.",
+  author: "Maxine Verner"
+},
+{
+  quote: "Why try to fit in when you can stand out like a flamingo in a flock of pigeons?",
+  author: "Flora Molder"
+},
+{
+  quote: "When life gives you melons, make melonade, then get treated for dyslexia.",
+  author: "Harry Truman"
+},
+{
+  quote: "If you can't find the key to success, just break down the door with a fireaxe. People probably won't mind.",
+  author: "Benny Soetoro"
+},
+{
+  quote: "Why settle for a silver lining when you can have a rainbow lining, and rainbow everything, all the time, forever?",
+  author: "Roxie Goettles"
+},
+{
+  quote: "Don't cry over spilled milk, just grab a mop and start dancing like the Grim Reaper.",
+  author: "Molly Freeman"
+},
+{
+  quote: "If you can't outrun the murderous horde, distract 'em with a glitter bomb and pray to Vishnu.",
+  author: "Steve from Tacoma"
+},
+{
+  quote: "Why swim upstream when you can just float downstream with a bunch of rubber ducks?",
+  author: "Steph Washinton"
+},
+{
+  quote: "If you can't decide between two options, just flip a coin and then do whatever you want.",
+  author: "Cathy Barton"
+},
+{
+  quote: "Why be a wallflower when you can be the life of the party, or the party itself? Invite yourself to your own party, then turn down the invite.",
+  author: "Parker Ranthrop" 
+},
+{
+  quote: "The only way to truly understand the universe is to become a sandwich.",
+  author: "Gerald McChicken"
+},
+{
+  quote: "Why climb a tree when you can just grow wings and fly into a volcano?",
+  author: "Dexter Barrens"
+},
+{
+  quote: "If at first you don't succeed, just keep failing until you reach 'success'.",
+  author: "Rebecca Crowder"
+},
+{
+  quote: "The only thing worse than being lost in the wilderness is being lost in your own mind. Like, whoa man.",
+  author: "Franklin Jickles"
+},
+{
+  quote: "Why run away from your problems when you can just stand still and let them run away from you?",
+  author: "Natalie Suna"
+},
+{
+  quote: "If you can't find your keys, just pretend you never had them in the first place.",
+  author: "Oliver Vausage"
+},
+{
+  quote: "The only way to truly understand the meaning of life is to live it backwards and upside down.",
+  author: "Gabrielle Dorngle"
+},
+{
+  quote: "Why chase rainbows when you can just build your own rainbow-chasing machine and rent it out for $79.99 plus tax?",
+  author: "Quincy Lamburger"
+},
+{
+  quote: "If you can't find your way, just ask the nearest squirrel for directions. Just know, he's probably lying.",
+  author: "Melissa Featball"
+},
+{
+  quote: "The only way to truly understand the beauty of the world is to squint your eyes and spin around in circles, crying. Trust me!",
+  author: "Henry D. Thumps"
+},
+{
+  quote: "Why follow the beaten path when you can just create your own path using a pogo stick and some mustard?",
+  author: "Isabella Zacon"
+},
+{
+  quote: "If you can't decide between two options, just choose both and then combine them into one super option. It never fails!",
+  author: "Winston Taffle"
+},
+{
+  quote: "The only way to truly understand the power of love is to sing it loudly and off-key into someone's face.",
+  author: "Jax Shepard III"
+},
+{
+  quote: "Why wait for opportunity to knock when you can just build your own door and invite it in? Be sure to kick it out at midnight though.",
+  author: "Benjamin Ragel"
+},
+{
+  quote: "If you can't find the words to express yourself, just speak in gibberish and let others interpret your meaning.",
+  author: "Avery Adams Jr."
+},
+{
+  quote: "The only way to truly guess at the meaning of success is to redefine it every day.",
+  author: "Maggie Nuffin"
+},
+{
+  quote: "Why settle for being a square when you can be a polygon with an infinite number of sides?",
+  author: "Leonard P. Yinguine"
+},
+{
+  quote: "If you can't find your way, just follow the nearest butterfly and see where it leads you. Unless it's heading to Hell, then don't go that way.",
+  author: "Oscar Viscette"
+},
+{
+  quote: "The only way to truly understand the concept of time is to pretend you're a clock and tick-tock your way through the workday.",
+  author: "Yvonne Semple"
+},
+{
+  quote: "Why be a cog in the machine when you can be the machine itself and grind your own rusty gears?",
+  author: "Trevor W. Hoast"
+}
+]
+};
+
+var colors = [
+'#E74C3C', // Alizarin
+'#8E44AD', // Wisteria
+'#1ABC9C', // Turquoise
+'#2ECC71', // Emerald
+'#F1C40F', // Sunflower
+'#E67E22', // Carrot
+'#3498DB', // Peter River
+'#9B59B6', // Amethyst
+'#16A085', // Green Sea
+'#F39C12', // Orange
+'#2980B9', // Belize Hole
+'#27AE60', // Nephritis
+];
+var currentQuote = '',
+  currentAuthor = '';
+ const image = document.createElement('img');
+
+function reload(){
+  $(window).on('load', function (e) {
+            $('#new-quote').on('click', function (e) {
+                $("#title").load(" #title > *");
+            });
+        });
+};
+
+function getPhoto() {
+  let stubUrl = 'https://boredhumans.b-cdn.net/faces2/'
+  let rando = Math.floor(Math.random() * 500)
+  let finalUrl = stubUrl.concat(`${rando}.jpg`)
+  image.src = finalUrl;
+  document.getElementById('photo').appendChild(image);
+}
+
+// function removePhoto() {
+//   const removeIt = document.getElementById('photo');
+//   removeIt.removeChild(removeIt.firstElementChild);
+// }
+
+function getQuotes() {
+   if (typeof coolQuotes === 'string'){
+    coolQuotes = JSON.parse(coolQuotes);
+   }
+};
+
+function getRandomQuote() {
+  return coolQuotes.quotes[
+    Math.floor(Math.random() * coolQuotes.quotes.length)
+  ];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+  //removePhoto();
+  getPhoto();
+  reload();
+
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  $('#tumblr-quote').attr(
+    'href',
+    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+      encodeURIComponent(currentAuthor) +
+      '&content=' +
+      encodeURIComponent(currentQuote) +
+      '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+  );
+
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 1000);
+    $('#text').text(randomQuote.quote);
+  });
+
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 1000);
+    $('#author').html(randomQuote.author);
+  });
+
+  $('#photo').animate({ opacity: 0 }, 01, function () {
+    $(this).animate({ opacity: 1 }, 4000);
+  });
+
+  var color = Math.floor(Math.random() * colors.length);
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  );
+  $('.button').animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  );
+}
+
+$(document).ready(function () {
+  getQuotes();
+  getQuote();
+  $('#new-quote').on('click', getQuote);
+});
